def58d4213248ab4b62a3b97b4e74e5db219f4a5
alexey <alexeyweb60@gmail.com>
2021-11-19 17:07:53 +0200

    Initial Commit

---
+++ diabet4/diabet4App.swift
@@ -0,0 +1,17 @@
+//
+//  diabet4App.swift
+//  diabet4
+//
+//  Created by alexey on 19.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct diabet4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
